edefd82de8c23f7dd45e6733d14dd5d02d488280
Jesse Leffew <jesse.leffew89@gmail.com>
2021-04-15 14:29:31 -0500

    change update

---
+++ js/type-operators.js
@@ -0,0 +1,88 @@
+"use strict";
+
+//var isTrue = true;
+//
+//console.log("The value of isTrue: " + isTrue);
+
+//---------------------- BOOLEAN
+
+//var iAmHungry = true;
+//
+//if (iAmHungry) {
+//    console.log("Lets find some food!");
+//}
+
+//var hoursSinceMeal = 5;
+//
+//var iamHungry = hoursSinceMeal > 4;
+//
+//if (iamHungry){
+//    console.log("Im getting a little hungry");
+//}
+
+//---------------------- NUMBERS
+
+//console.log("The number is: = 10");
+//console.log("The number is: + 9984379827");
+//console.log("The number is: + -1339");
+//console.log("The number is: + 13.4");
+
+//console.log(10+10); //addition operator
+//
+//console.log(3-6); // subtraction operator
+//
+//console.log(5*4); // multiplication operator
+//
+//console.log(1/0); // division operator
+//
+//console.log((-0) / 0);
+//
+//console.log(15 % 6);
+
+// TODO: console.log() five equations using each arithmetic operator. Each equation should resolve 4.
+
+//console.log(2+2);
+//
+//console.log(14-10);
+//
+//console.log(2*2);
+//
+//console.log(24/6);
+//
+//console.log(36%8);
+
+//--------------------UNDEFINED
+
+// a variable/object which is written but not defined
+
+var name;
+
+//console.log(name);
+
+//---------------------STRINGS
+
+//console.log("Jesse's pretty hungry"); // string literal
+//
+//console.log('Jesse\'s pretty hungry');
+
+//name = "Jesse";
+//var status = "\n\tis\n\t\thungry";
+//console.log(name + status);
+
+//------------------TYPE OF
+//var name;
+//
+//console.log("The type is: " + typeof(4 *"7"));
+//
+//console.log("The type is: " + typeof(name));
+//
+//console.log("The type is: " + typeof(null));
+
+
+
+
+
+
+
+
+
---
+++ js/variables-review.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//VARIABLES
+// A variable is a symbolic representative of a value that can be different in each run of a program
+// The variable is a pointer to a stored place in memory of your runny application
+
+var name = "Jesse";
+var expectedName = "Jesse";
+
+if (name === expectedName){
+    console.log("You got the right name!");
+}
