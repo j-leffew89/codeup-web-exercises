b117a15be76577ea460dabc4173ae825121eb58e
Jesse Leffew <jesse.leffew89@gmail.com>
2021-04-14 22:30:32 -0500

    update external.js

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+
+alert("Welcome to my Website");
+
+var userInput = prompt("What is your favorite color?");
+
+if (userInput.toLowerCase() === "green"){
+    alert("That's my favorite color too!");
+}else {
+    alert("Awesome mine too!");
+}
