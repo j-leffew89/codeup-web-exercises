c39509ccab103ecb42a51f801627c0a3b3a4d42b
Jesse Leffew <jesse.leffew89@gmail.com>
2021-04-19 00:10:32 -0500

    new

--- js_exercises/Final-exercises.js
+++ js_exercises/Final-exercises.js
@@ -1,21 +1,58 @@
-// var sample = "Hello Codeup";
-//
-// console.log(sample.length);
-//
-// console.log(sample.toUpperCase());
-//
-// var sampleStudents = sample + "Students";
-// console.log(sampleStudents);
-//
-// console.log (sampleStudents.replace(" Students ", " Class "));
-//
-// console.log(sampleStudents.indexOf("c"));
-//
-// console.log(sampleStudents.indexOf("C"));
-//
-// console.log(sampleStudents.indexOf("Codeup"));
+ var sample = "Hello Codeup";
 
-//Exercise #3
+ console.log(sample.length);
+
+ console.log(sample.toUpperCase());
+
+var sampleStudents = sample + "Students";
+ console.log(sampleStudents);
+
+ console.log (sampleStudents.replace(" Students ", " Class "));
+
+ console.log(sampleStudents.indexOf("c"));
+
+console.log(sampleStudents.indexOf("C"));
+
+ console.log(sampleStudents.indexOf("Codeup"));
+
+//Exercise #3 -----1
+
+var moviePrice = 3;
+var littleMermaid = (moviePrice * 3);
+var brotherBear = (moviePrice * 5);
+var hercules = 3;
+
+var totalAmount = (littleMermaid + brotherBear + hercules);
+
+ console.log(totalAmount);
+
+ // -------2
+
+ var google = (400 * 6);
+ var faceBook = (350 * 10);
+ var amazon = (380 * 4);
+
+ var totalPay = (google + amazon + faceBook);
+
+ console.log(totalPay);
+
+ // -------- 3
+
+ var enroll
+    function
+
+
+ }
+
+ // ---------- 4
+
+var productOffer
 
 
 
+ // Exercise 4
+
+ var username = 'codeup';
+var password = 'notastrongpassword';
+
+var
