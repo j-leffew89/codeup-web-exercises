1a2707cb493659af3450ebbea61b8136864c61fa
Jesse Leffew <jesse.leffew89@gmail.com>
2021-04-23 20:51:50 -0500

    loops

---
+++ js/for_loops.js
@@ -0,0 +1,62 @@
+function showMultiplicationTable(num){
+    for(let i = 1; i <=10; i++){
+        console.log(`${num} * ${i} = ${num * i}`);
+
+    }
+}
+
+showMultiplicationTable(7);
+
+
+
+//----------------------------------------------
+
+
+for(var i = 0; i < 10; i++) {
+    var randomNum = Math.floor(Math.random() * (200 - 2 + 1) + 200);
+    console.log((`${randomNum} at iteration: $ {i}`))
+}
+
+
+
+
+
+    // let table = 7;
+    // let count = 10;
+    //
+    // for (let i = 0; i < count; i++) {
+    //     i++;
+    //     console.log(table, "X", i, "=", table * i);
+    //     --i;
+    // }
+    //
+
+
+//-----------------------------
+
+ // for(let randomNum % 2 === 0){
+ //        console.log("Even") {
+ //        }else {
+ //            console.log("Odd")
+ //        }
+ //    }
+ //
+
+
+
+
+
+
+
+// //-------------------------------
+// for(let i = 1; i <= 9; i++) {
+//     var arr = []
+//     for (let x = 1; x <= i; x++) {
+//         arr.push(i);
+//     }
+//     console.log(arr.join(""));
+// }
+//
+// let numberPyramid = function(start, end){
+//     for(let i = start)
+// }
