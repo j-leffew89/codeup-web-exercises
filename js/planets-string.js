53915b0cdc29f93f665e855daf2f1eba1326f21e
Jesse Leffew <jesse.leffew89@gmail.com>
2021-04-26 18:36:42 -0500

    planets-string

---
+++ js/planets-string.js
@@ -0,0 +1,28 @@
+(function() {
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray = planetsString.split(',');
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */
+
+
+ console.log(planetsArray);
+
+//     var namesString = "Joe,Bob,Sally";
+//
+//     console.log(namesString);
+// // Joe,Bob,Sally
+//
+//     var namesArray = namesString.split(',');
+//
+//     console.log(namesArray);
+
+
+
+})();
