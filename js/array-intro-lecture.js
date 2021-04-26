c1461f972094744dd99a10bb12325a8f4a94cddf
Jesse Leffew <jesse.leffew89@gmail.com>
2021-04-26 16:59:54 -0500

    update

--- js/array-intro-lecture.js
+++ js/array-intro-lecture.js
@@ -18,18 +18,69 @@
 //  -> DID YOU KNOW: You can add different data types to an array??
 //      -> Try adding strings, numbers, or booleans as elements
 
-    const sports = [`Football`, `Basketball`, `Baseball`, `Soccer`, `Boxing`];
-
-        for (let i = 0; i < sports.length; i++) {
-            console.log(sports[i]);
-     }
-    // console.log(sports[0]);
+//     const sports = [`Football`, `Basketball`, `Baseball`, `Soccer`, `Boxing`];
 //
+//         for (let i = 0; i < sports.length; i++) {
+//             console.log(sports[i]);
+//      }
+//     // console.log(sports[0]);
+// //
+//
+//     sports.forEach(function(sports ){
+//         console.log(`The most watch sports are: ` + sports);
+//
+//
+//     })
 
-    sports.forEach(function(sports ){
-        console.log(`The most watch sports are: ` + sports);
+    // TODO: MINI-EXERCISE
+//  -> var days = ["Monday", "Tuesday", "Wednesday"];
+//  -> Create an array with the remaining days of the week
+//  -> Call .forEach() on that NEW array
+//  -> Inside the inline function you pass into .forEach():
+//      -> add the remainingDay of the week to the old array
+//  -> When the loop completes, console.log() the completed day of the week array
+
+    //
+    // const days = [`Monday`, `Tuesday`, `Wednesday`];
+    //
+    // const remainingDays = [`Thursday`, `Friday`, `Saturday`,`Sunday`];
+    // remainingDays.forEach(function (day){
+    //     days.push(day);
+    // });
+    // console.log(days)
+
+// TODO - MINI EXERCISE
+//  -> Let's reverse engineer our days of the week array
+//  -> Make sure to console.log in order to see your code's behavior!
+//  -> Write a function which takes in your complete days of the week array
+//      -> It will eventually return your reverse-engineered array
+//  -> Using a FOR LOOP (not .forEach())
+//      -> pop() each item off the days array
+//      -> use the returned value from pop() to unshift the element
+//      -> on to the new array
+//  -> HINT: You may find that the loop doesn't behave as expected
+//      -> console.log EVERY possible thing which could change as your loop runs
+//          -> consider how the methods yoy are using change state of the array
+
+    let days = [`Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`, `Sunday`];
+
+    function completeDays(oldArray) {
+        let newArray = [];
+        let oldArrayLength = oldArray.length
+        for (let i = 0; i < oldArrayLength; i++) {
+
+            let removedDays = oldArray.pop();
+            newArray.unshift(removedDays)
+            console.log(newArray);
+            //console.log(`days left: ` + i);
+
+        }
+        console.log(oldArray)
+    }
+    completeDays(days);
+    // days.reverse();
+    // console.log(days);
 
 
-    })
 
 })();
--- js/iterating.js
+++ js/iterating.js
@@ -29,6 +29,7 @@
     console.log( name [1]);
     console.log( name [2]);
     console.log( name [3]);
+    console.log( name [4]);
 
     /**
      * TODO:
