5a8b500fd91844397607c17cd289d413fe6cc232
Jesse Leffew <jesse.leffew89@gmail.com>
2021-05-03 09:28:35 -0500

    function trim

---
+++ js/morning-practice.js
@@ -0,0 +1,8 @@
+//Make a function named trim(string) that removes empty spaces before and after the input.
+
+function trim(input){
+    return input.replaceAll(' ', '');
+
+}
+
+console.log(trim('I had pizza for dinner'));
