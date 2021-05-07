0a7594101403a224ee21a67f65a0e280380855d7
Jesse Leffew <jesse.leffew89@gmail.com>
2021-05-06 20:22:13 -0500

    update everything

--- codeup-web-exercises/js/conditionals.js
+++ codeup-web-exercises/js/conditionals.js
@@ -1,10 +1,10 @@
 (function() {
     "use strict";
 
-//
-//     /* ########################################################################## */
-//
-//     /**
+
+    /* ########################################################################## */
+
+    /**
 //      * TODO:
 //      * Create a function named `analyzeColor` that accepts a string that is a color
 //      * name as input. This function should return a message that related to that
@@ -22,82 +22,82 @@
 //      * console.logging the function's return value
 //      */
 //
-//     function analyzeColor(input) {
-//         console.log(input)
-//
-//         if (input === 'blue') {
-//             console.log("Blue is the color of the sky");
-//         } else if (input === 'red') {
-//             console.log("strawberry is red");
-//         } else if (input === 'cyan') {
-//             console.log("I dont know anything about cyan");
-//         }
-//
-//     }
-//
-//     analyzeColor('red');
-//
-//
-// // Don't change the next two lines!
-// // These lines create two variables for you:
-// // - `colors`: a list of the colors of the rainbow
-// // - `randomColor`: contains a single random color value from the list (this
-// //                  will contain a different color every time the page loads)
-//     var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-//     var randomColor = colors[Math.floor(Math.random() * colors.length)];
-//     /**
-//      * TODO:
-//      * Pass the `randomColor` variable to your function and console.log the results.
-//      * You should see a different message every time you refresh the page
-//      */
-//
-//     console.log(randomColor);
-//
-//
-//     /**
-//      * TODO:
-//      * Refactor your above function to use a switch-case statement
-//      */
-//     function analyzeColors(colors) {
-//
-//         switch (colors) {
-//
-//             case"blue":
-//                 console.log("blue is the color of the sky!");
-//                 break;
-//             case"red":
-//                 console.log("Strawberry is red");
-//                 break;
-//             case"cyan":
-//                 console.log("I don't know anything about cyan");
-//                 break;
-//
-//
-//         }
-//     }
-//
-//     analyzeColors("cyan");
-//
-//     /**
-//      * TODO:
-//      * Prompt the user for a color when the page loads, and pass the input from the
-//      * user to your `analyzeColor` function. Alert the return value from your
-//      * function to show it to the user.
-//      */
-//
-//         console.log(prompt ("What color will you pick. Blue, red or green"));
-//             let colors;
-//         {
-//             if (colors === 'blue') {
-//                 alert("Blue is the color of the sky");
-//             } else if (colors === 'red') {
-//                 alert("strawberry is red");
-//             } else if (colors === 'cyan') {
-//                 alert("I dont know anything about cyan");
-//             }
-//         }
-//         console.log(analyzeColor(colors));
-    /* ########################################################################## */
+     function analyzeColor(input) {
+        console.log(input)
+
+         if (input === 'blue') {
+             console.log("Blue is the color of the sky");
+         } else if (input === 'red') {
+             console.log("strawberry is red");
+         } else if (input === 'cyan') {
+             console.log("I dont know anything about cyan");
+         }
+
+     }
+
+     analyzeColor('red');
+
+
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color every time the page loads)
+    var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    /**
+     * TODO:
+     * Pass the `randomColor` variable to your function and console.log the results.
+     * You should see a different message every time you refresh the page
+     */
+
+    console.log(randomColor);
+
+
+    /**
+     * TODO:
+     * Refactor your above function to use a switch-case statement
+     */
+    function analyzeColors(colors) {
+
+        switch (colors) {
+
+            case"blue":
+                console.log("blue is the color of the sky!");
+                break;
+            case"red":
+                console.log("Strawberry is red");
+                break;
+            case"cyan":
+                console.log("I don't know anything about cyan");
+                break;
+
+
+        }
+    }
+
+    analyzeColors("cyan");
+
+    /**
+     * TODO:
+     * Prompt the user for a color when the page loads, and pass the input from the
+     * user to your `analyzeColor` function. Alert the return value from your
+     * function to show it to the user.
+     */
+
+        console.log(prompt ("What color will you pick. Blue, red or green"));
+            let colors;
+        {
+            if (colors === 'blue') {
+                alert("Blue is the color of the sky");
+            } else if (colors === 'red') {
+                alert("strawberry is red");
+            } else if (colors === 'cyan') {
+                alert("I dont know anything about cyan");
+            }
+        }
+         console.log(analyzeColor(colors));
+     /* ########################################################################## */
 
     /**
      * TODO:
