af93aeb97af3e904cdb66cd8ca0435be1776247f
Jesse Leffew <jesse.leffew89@gmail.com>
2021-05-17 23:30:00 -0500

    practice

--- js/for_loops.js
+++ js/for_loops.js
@@ -46,3 +46,10 @@ for(let i = 100; i >= 5; i -= 5){
 //
 // console.log(capitalizeWords(sentence));
 
+console.log(`-------------------------------------------`)
+
+for(let i = 1; i <= 10; i++) {
+    // if (i % 2 === 1){
+        console.log(i)
+}
+
--- js/morning-practice.js
+++ js/morning-practice.js
@@ -13,14 +13,30 @@
 //Write a function named cubeRoot(n) that returns the cube root of the input
 
 
-function cube(n) {
-    return Math.pow(n,3)
+// function cube(n) {
+//     return Math.pow(n,3)
+// }
+//
+// console.log(cube(3))
+//
+//
+// function cubeRoot (n){
+//     return Math.cbrt(n)
+// };
+// console.log(Math.cbrt(cubeRoot(3)))
+
+// Write a function named secondToLast(arr) that returns the data type of the
+// second to last element of the array parameter. When you call the function,
+// pass in an array of your choosing
+
+// function secondToLast(arr){
+//     let secondToLast = [null, NaN, "boolean", "Numbers"]
+//      return arr(arr.length -2)
+// }
+
+let numbers = ["jesse", 0, NaN, true, 8,null]
+function secondToLast(names){
+    return typeof names[names.length -4]
 }
 
-console.log(cube(3))
-
-
-function cubeRoot (n){
-    return Math.cbrt(n)
-};
-console.log(Math.cbrt(cubeRoot(3)))
+console.log(secondToLast(numbers));
