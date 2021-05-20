3b985a648689860018f2814f90a39573639acde4
Jesse Leffew <jesse.leffew89@gmail.com>
2021-05-20 16:27:19 -0500

    work

--- js/DOM.js
+++ js/DOM.js
@@ -7,13 +7,32 @@
 
 // exercise 1. write a function called makeCar that takes intwo strings
 
-const convert = (str) => {
-    const obj = {}
-    str.split("").forEach((letter, index) => {
-        obj[index] = letter
-        obj[letter] = letter
-    });
-    return obj
-}
+// const convert = (str) => {
+//     const obj = {}
+//     str.split("").forEach((letter, index) => {
+//         obj[index] = letter
+//         obj[letter] = letter
+//     });
+//     return obj
+// }
+//
+// console.log(convert("string"))
 
-console.log(convert("string"))
+// let newDiv = document.createElement("div");
+// console.log(newDiv);
+//
+// let newUl = document.createElement("ul");
+// console.log(newUl);
+//
+// newDiv.innerText = "This is a brand new div";
+// console.log(newDiv);
+//
+// newDiv.innerHTML = "<li> Test 1</li> <li>Test 2</li>"
+// console.log(newDiv);
+
+// TODO: change the text inside of 1 of the paragraphs to be a short paragraph about you
+// TODO: log the second paragraph element from the list of all
+//  paragraphs
+// TODO: create a new ul element and 5 new li elements. Each of your li's should have some text inside of it
+// TODO: replace the last div's paragraph with your new ul. make sure the ul has all 5 li's inside of it
+// TODO: remove the last li
