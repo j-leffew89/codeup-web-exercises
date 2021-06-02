a301674edce417c09110f1e61f770763934793c3
Jesse Leffew <jesse.leffew89@gmail.com>
2021-06-02 12:30:45 -0500

    jquery exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+ $('#header').css('color', 'green')
+alert($('#header')[0].innerText);
+ $('.dragon').css('font-size', '35px')
+$('.codeup').css('border', '1px solid blue')
+$('li').css('font-size', '20px')
+$('div').css({
+ "font-variant-numeric": 'ordinal',
+    'text-align': '-webkit-center'
+})
+$('#list').css({
+    'color': 'darkblue',
+    'list-style': 'none'
+})
+
+$('.craft').css({
+    "background-color": "#fff2ac",
+
+});
+
+ alert($('h1')[0].innerText)
