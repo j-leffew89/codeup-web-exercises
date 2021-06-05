6a934364f7822fd44433d3c97bf7892aa5fdeef5
Jesse Leffew <jesse.leffew89@gmail.com>
2021-06-05 16:31:23 -0500

    jquery-faq part one

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,41 +1,41 @@
-// $(document).ready(function() {
-//     alert( 'The DOM has finished loading!' );
-// });
-//
-//  $('#header').css('color', 'green')
-// alert($('#header')[0].innerText);
-//  $('.dragon').css('font-size', '35px')
-// $('.codeup').css('border', '1px solid blue')
-// $('li').css('font-size', '20px')
-// $('div').css({
-//  "font-variant-numeric": 'ordinal',
-//     'text-align': '-webkit-center'
-// })
-// $('#list').css({
-//     'color': 'darkblue',
-//     'list-style': 'none'
-// })
-//
-// $('.craft').css({
-//     "background-color": "#fff2ac",
-//
-// });
-//
-//  alert($('h1')[0].innerText)
-//
+$(document).ready(function() {
+    alert( 'The DOM has finished loading!' );
+});
 
-$(document).ready(function (){
-    $('h1').on('click',function (){
+ $('#header').css('color', 'green')
+alert($('#header')[0].innerText);
+ $('.dragon').css('font-size', '35px')
+$('.codeup').css('border', '1px solid blue')
+$('li').css('font-size', '20px')
+$('div').css({
+ "font-variant-numeric": 'ordinal',
+    'text-align': '-webkit-center'
+})
+$('#list').css({
+    'color': 'darkblue',
+    'list-style': 'none'
+})
+
+$('.craft').css({
+    "background-color": "#fff2ac",
+
+});
+
+ alert($('h1')[0].innerText)
+
+
+$(document).ready(function () {
+    $('h1').on('click', function () {
         $(this).css('background-color', 'blue');
     });
 });
 
-$('p').dblclick(function() {
+$('p').dblclick(function () {
     $(this).css('font-size', '18px');
 });
 
-$('.jqClass').hover(function () {
+$('li').hover(function () {
     $(this).css('color', 'red');
-},function(){
+}, function () {
     $(this).css('color', 'black');
 });
