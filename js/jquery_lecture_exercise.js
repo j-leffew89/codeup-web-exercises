dd6f18673c74c13cb3019e8ec02ed54e4dbecf6f
Jesse Leffew <jesse.leffew89@gmail.com>
2021-06-07 16:06:38 -0500

    more jquery

--- js/jquery_lecture_exercise.js
+++ js/jquery_lecture_exercise.js
@@ -57,13 +57,13 @@
 //     })
 //
 //
-//     $(document).ready(function () {
-//         $('#main-title').click(function () {
-//             let font = $(this).css('font-size');
-//             let doubleFont = parseFloat(font) * 2;
-//             $(this).css('font-size', doubleFont);
-//         });
-//     });
+    $(document).ready(function () {
+        $('#main-title').click(function () {
+            let font = $(this).css('font-size');
+            let doubleFont = parseFloat(font) * 2;
+            $(this).css('font-size', doubleFont);
+        });
+    });
 //
 
 //     let hoverIn = function () {
@@ -84,32 +84,32 @@
 //      -> Don't worry about changing the text back
 
 
-$('.list-group-item').click(function () {
-    if ($(this).css('background-color') === 'rgb(255, 192, 203)') {
-        $(this).css('background-color', "")
-    } else {
-        $(this).css({
-            'background-color': 'pink',
-            'text-transform': 'uppercase'
-        });
-    }
-});
+// $('.list-group-item').click(function () {
+//     if ($(this).css('background-color') === 'rgb(255, 192, 203)') {
+//         $(this).css('background-color', "")
+//     } else {
+//         $(this).css({
+//             'background-color': 'pink',
+//             'text-transform': 'uppercase'
+//         });
+//     }
+// });
 
 // TODO: Change up the submit button event
 //  -> Add a new input with an id of #redirect-url
 //  -> Remove the old click events from #submitBtn
 //  -> When the user clicks #submitBtn, redirect the page to the value of #redirect-url
 //  -> HINT: You can either add a new input or change the id of an existing input element to #redirect-url to save time
-
-$('#redirect-url')(function () {
-    window.location.replace('http://tutorialrepublic.com/');
-})
-
-setTimeout('redirect-url()', 10000)
-
-
-// TODO: After a 2 second delay (BOM), when the user loads the page, change #main-title to a value of your choosing (change text, background color, what have you)!
-setTimeout(function (){
-    $('#main-title').css({'background-color': 'blue'
-    'color': 'black'});
-});
+//
+// $('#redirect-url')(function () {
+//     window.location.replace('http://tutorialrepublic.com/');
+// })
+//
+// setTimeout('redirect-url()', 10000)
+//
+//
+// // TODO: After a 2 second delay (BOM), when the user loads the page, change #main-title to a value of your choosing (change text, background color, what have you)!
+// setTimeout(function (){
+//     $('#main-title').css({'background-color': 'blue',
+//     'color': 'black'});
+// });
