d9a2fd7e158135dde791f3f3afa2dcfb3279e9d4
Jesse Leffew <jesse.leffew89@gmail.com>
2021-06-07 13:31:25 -0500

    jquery

--- js/jquery_lecture_exercise.js
+++ js/jquery_lecture_exercise.js
@@ -1,165 +1,115 @@
-// (function(){
+//
+// (function () {
 //     let jqElements = $('.jqClass');
 //     let backgroundBlue = 'lightblue';
 //     let backgroundReset = '';
-//     function addClickEvents(elementsToBind, color1, color2){
-//         let handlerBlueBckGnd = function(){
+//
+//     function addClickEvents(elementsToBind, color1, color2) {
+//         let handlerBlueBckGnd = function () {
 //             elementsToBind.css('background-color', color1);
 //             $(this).css('background-color', color2);
 //         }
 //         elementsToBind.click(handlerBlueBckGnd);
 //     }
+//
 //     addClickEvents(jqElements, backgroundReset, backgroundBlue);
 //     addClickEvents($(".card"), "", "red");
-//     //make sure the DOM is loaded first
-//     // document.addEventListener("DOMContentLoaded", function() {
-//     //     addListeners();
-//     // });
 //
-//     $(document).ready(function(){
-//         addListeners();
-//     });
 //
-//     //parent method for adding all listeners
+//     addListeners();
+//
 //     function addListeners() {
-//         //get the elements to add listener and change image
-//         //     let cardContainers = Array.from($(".col-md-3"));
-//         //     addEvents(cardContainers, 'img/jazz-music-rubber-duck.jpg', 'mouseover');
-//         //     addEvents(cardContainers, 'img/question.png', 'mouseout');
-//         // }
+//         let cardContainers = $('.col-md-3');
+//         addEvents(cardContainers, 'img/jazz-music-rubber-duck.jpg', 'img/question.png')
 //     }
-//     //add mouseover/mouseout events to all selected elements
-//     function addEvents(cardContainers, imgPath, listenerType){
-//         cardContainers.forEach((cc) => {
-//             let card = cc;
-//             //create listener which will call function change the image
-//             let listener = function (event) {
-//                 // let cardImg = card.querySelector(".card .card-img-top");
-//                 // changeImage(cardImg, imgPath);
-//                 jqElements.hover(function () {
-//                     $(this).image('img/jazz-music-rubber-duck.jpg')
-//                 }, function () {
-//                     $(this).image('img/question.png')
-//                 });
-//             };
-//             cc.addEventListener(listenerType, listener);
-//         });
-//     }
-//     //actual function to swap images
-//     function changeImage(card, imgPath){
-//         card.setAttribute('src', imgPath);
+//
+//     function addEvents(cardContainers, imgPath1, imgPath2) {
+//
+//
+//         let handler1 = function (event) {
+//             $(this)
+//                 .find('.card-img-top')
+//                 .attr('src', imgPath1);
+//         };
+//
+//         let handler2 = function (event) {
+//             $(this)
+//                 .find('.card-img-top')
+//                 .attr('src', imgPath2)
+//         };
+//         cardContainers.hover(handler1, handler2);
 //     }
 //
 // })();
-
-
-(function () {
-    let jqElements = $('.jqClass');
-    let backgroundBlue = 'lightblue';
-    let backgroundReset = '';
-
-    function addClickEvents(elementsToBind, color1, color2) {
-        let handlerBlueBckGnd = function () {
-            elementsToBind.css('background-color', color1);
-            $(this).css('background-color', color2);
-        }
-        elementsToBind.click(handlerBlueBckGnd);
-    }
-
-    addClickEvents(jqElements, backgroundReset, backgroundBlue);
-    addClickEvents($(".card"), "", "red");
-//     //make sure the DOM is loaded first
-//     document.addEventListener("DOMContentLoaded", function() {
-//         addListeners();
+//
+//
+// $(document).ready(function () {
+//     $('#submitBtn').click(function () {
+//         $('#override-bootstrap').css('background-color', 'red')
 //     });
-//     //parent method for adding all listeners
-//     function addListeners(){
-//         //get the elements to add listener and change image
-//         let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
-//         addEvents(cardContainers, 'assets/jazz-music-rubber-duck.jpg', 'mouseover');
-//         addEvents(cardContainers, 'assets/question.png', 'mouseout');
-//     }
-//     //add mouseover/mouseout events to all selected elements
-//     function addEvents(cardContainers, imgPath, listenerType){
-//         cardContainers.forEach((cc) => {
-//             let card = cc;
-//             //create listener which will call function change the image
-//             let listener = function (event) {
-//                 let cardImg = card.querySelector(".card .card-img-top");
-//                 changeImage(cardImg, imgPath);
-//             };
-//             cc.addEventListener(listenerType, listener);
+//
+//     $('.card-text').click(function () {
+//         $(this).css('background-color', 'red')
+//     });
+//
+//     $('#restBtn').click(function () {
+//         location.reload()
+//     })
+//
+//
+//     $(document).ready(function () {
+//         $('#main-title').click(function () {
+//             let font = $(this).css('font-size');
+//             let doubleFont = parseFloat(font) * 2;
+//             $(this).css('font-size', doubleFont);
 //         });
+//     });
+//
+
+//     let hoverIn = function () {
+//         let attributeValue = $(this).attr('date-attribute');
+//         $(this).find('.card-title').text(attributeValue);
+//     };
+//
+//     let hoverOut = function () {
+//         $(this).find('.card-title').text('card');
 //     }
-//     //actual function to swap images
-//     function changeImage(card, imgPath){
-//         card.setAttribute('src', imgPath);
-//     }
+//
+//     $('.col-md-3').hover(hoverIn, hoverOut)
+// });
 
-    addListeners();
-
-    function addListeners() {
-        let cardContainers = $('.col-md-3');
-        addEvents(cardContainers, 'img/jazz-music-rubber-duck.jpg', 'img/question.png')
-    }
-
-    function addEvents(cardContainers, imgPath1, imgPath2) {
-        // let handler1 = function (event) {
-        //     let cardImg = $(this)
-        //         .find('.card .card-img-top');
-        //     changeImage(cardImg, imgPath1)
-        // };
-        //
-        // let handler2 = function (event) {
-        //     let cardImg = $(this)
-        //         .find('.card .card-img-top');
-        //     changeImage(cardImg, imgPath2)
-        // };
-        //
-        // function changeImage(cardImg, imgPath) {
-        //     cardImg.attr('src', imgPath);
-        // }
-
-        let handler1 = function (event) {
-            $(this)
-                .find('.card-img-top')
-                .attr('src', imgPath1);
-        };
-
-        let handler2 = function (event) {
-            $(this)
-                .find('.card-img-top')
-                .attr('src', imgPath2)
-        };
-        cardContainers.hover(handler1, handler2);
-    }
-
-})();
+// TODO: When an li with the .list-group-item class is clicked, replace the text with the same text, but uppercased
+//  -> ALSO, change the background of the li to a new color
+//  -> When the li is clicked again, reset the li background color to its original color
+//      -> Don't worry about changing the text back
 
 
-
-$(document).ready(function (){
-    $('#submitBtn').click(function () {
-        $('#override-bootstrap').css('background-color', 'red')
-    });
-
-    $('.card-text').click(function (){
-        $(this).css('background-color', 'red')
-    });
-
-    $('#restBtn').click(function (){
-        location.reload()
-    })
-
-    // $('#main-title').click(function() {
-    //     $(this).css('font-size', '4em');
-    // });
-    $(document).ready(function (){
-        $('#main-title').click(function (){
-            let font = $(this).css('font-size');
-            let doubleFont = parseFloat(font) * 2;
-            $(this).css('font-size', doubleFont);
+$('.list-group-item').click(function () {
+    if ($(this).css('background-color') === 'rgb(255, 192, 203)') {
+        $(this).css('background-color', "")
+    } else {
+        $(this).css({
+            'background-color': 'pink',
+            'text-transform': 'uppercase'
         });
-    });
+    }
+});
 
+// TODO: Change up the submit button event
+//  -> Add a new input with an id of #redirect-url
+//  -> Remove the old click events from #submitBtn
+//  -> When the user clicks #submitBtn, redirect the page to the value of #redirect-url
+//  -> HINT: You can either add a new input or change the id of an existing input element to #redirect-url to save time
+
+$('#redirect-url')(function () {
+    window.location.replace('http://tutorialrepublic.com/');
+})
+
+setTimeout('redirect-url()', 10000)
+
+
+// TODO: After a 2 second delay (BOM), when the user loads the page, change #main-title to a value of your choosing (change text, background color, what have you)!
+setTimeout(function (){
+    $('#main-title').css({'background-color': 'blue'
+    'color': 'black'});
 });
